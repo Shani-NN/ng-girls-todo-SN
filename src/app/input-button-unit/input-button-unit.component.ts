23a59eae3a409b71532e0ddb71d5eed3262748b5
Shani Negron <shani.negron@gmail.com>
2020-07-26 12:35:04 -0500

    Styles

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   template: `
 
 
-    <input #inputElementRef
-    [value]="title"
-    (keyup.enter)="submitValue($event.target.value)">
+    <input class="todo-input"
+          #inputElementRef
+          [value]="title"
+          (keyup.enter)="submitValue($event.target.value)">
 
-    <button (click)="submitValue(inputElementRef.value)">
+    <button class="btn"
+            (click)="submitValue(inputElementRef.value)">
       Save
     </button>
   `,
--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -3,14 +3,17 @@ import { TodoItem } from '../interfaces/todo-item';
 @Component({
   selector: 'app-list-manager',
   template: `
-  <todo-input-button-unit (submit)="addItem($event)">
-  </todo-input-button-unit>
+  <div class="todo-app">
+
+    <todo-input-button-unit (submit)="addItem($event)">
+    </todo-input-button-unit>
 
   <ul>
     <li *ngFor="let todoItem of todoList">
       <todo-todo-item [item]="todoItem"></todo-todo-item>
     </li>
   </ul>
+</div>
     `,
   styleUrls: ['./list-manager.component.css']
 })
